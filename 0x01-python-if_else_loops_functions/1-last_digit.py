5eeba02430558fb287dd667756c793d1e04114ec
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-10 01:48:52 +0200

    Fix negative numbers and pycodestyle

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import random
+
 number = random.randint(-10000, 10000)
-lastDigit = number
+lastDigit = abs(number) % 10
+text = " and is less than 6 and not 0"
+
 if lastDigit > 5:
-    print(f"Last digit of {number} is "+ str(lastDigit)[-1:] +" and is greater than 5")
+    print(f"Last digit of {number} is {lastDigit} and is greater than 5")
 elif lastDigit == 0:
-    print(f"Last digit of {number} is "+ str(lastDigit)[-1:] +" and is 0")
-elif lastDigit < 6 & lastDigit > 0:
-    print(f"Last digit of {number} is "+ str(lastDigit)[-1:] +" and is less than 6 and not 0")
+    print(f"Last digit of {number} is {lastDigit} and is 0")
+elif 0 < lastDigit < 6 and number > 0:
+    print(f"Last digit of {number} is {lastDigit}{text}")
 else:
-    print(number)
+    print(f"Last digit of {number} is -{lastDigit}{text}")
