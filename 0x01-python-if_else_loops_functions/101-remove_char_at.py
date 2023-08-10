0cfbf1df2f9912d96852e0cbe29fa245175c9d0b
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-10 23:41:20 +0200

    Fix if n is negative

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if n < abs(len(str)):
+    if n >= 0:
         return str[:n] + str[n+1:]
-    elif n == -n:
-        return str
     else:
         return str
