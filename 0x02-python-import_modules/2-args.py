94f880f9d53c49757d7bf253bd4e0ed764f21f2e
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-11 17:34:54 +0200

    Fix executing module as script

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-import sys
-argv = sys.argv[1:]
-n = len(argv)
-numList = []
-print("{} arguments:".format(n))
+def print_args():
+    import sys
+    argv = sys.argv[1:]
+    n = len(argv)
+    numList = []
 
-n = len(argv) + 1
+    if n == 0:
+        print("{} arguments.".format(n))
+    else:
+        print("{} arguments:".format(n))
+    
+    n = len(argv) + 1
+    for i in range(1, n):
+        numList.append(i)
+    for x, y in zip(numList, argv):
+        print("{}: {}".format(x, y))
 
-for i in range(1, n):
-    numList.append(i)
-
-for x, y in zip(numList, argv):
-    print("{}: {}".format(x, y))
+if __name__ == "__main__":
+    print_args()
