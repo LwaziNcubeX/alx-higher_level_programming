b1fe4370ed3c860697ec93f6aea1792474947fec
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-11 17:37:20 +0200

    Fix pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,12 +9,13 @@ def print_args():
         print("{} arguments.".format(n))
     else:
         print("{} arguments:".format(n))
-    
+
     n = len(argv) + 1
     for i in range(1, n):
         numList.append(i)
     for x, y in zip(numList, argv):
         print("{}: {}".format(x, y))
+
 
 if __name__ == "__main__":
     print_args()
