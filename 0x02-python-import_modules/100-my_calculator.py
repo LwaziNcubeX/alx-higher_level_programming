32bd4912c005a48c742ba6284638e3e636fd1de4
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-11 23:28:22 +0200

    handles basic operations.

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from sys import exit
+    from calculator_1 import add, sub, mul, div
+    argv = sys.argv
+    n = len(argv)
+
+    error = "Unknown operator. Available operators:"
+    if n < 4:
+        exit("Usage: ./100-my_calculator.py <a> <operator> <b>")
+    else:
+        a = int(argv[1])
+        b = int(argv[3])
+        op = argv[2]
+        if op == "+":
+            print("{} {} {} = {}".format(a, op, b, add(a, b)))
+        elif op == "-":
+            print("{} {} {} = {}".format(a, op, b, sub(a, b)))
+        elif op == "*":
+            print("{} {} {} = {}".format(a, op, b, mul(a, b)))
+        elif op == "/":
+            print("{} {} {} = {}".format(a, op, b, div(a, b)))
+        else:
+            exit("{} +, -, * and /".format(error))
