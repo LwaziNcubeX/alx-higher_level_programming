e060746dddf810e879cd8a5e0b0d62dc70b1a210
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-13 17:33:50 +0200

    .

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,6 +5,7 @@ def replace_in_list(my_list, idx, element):
     elif idx > (len(my_list)):
         return my_list
     else:
-        del my_list[idx]
-        new_list = my_list.insert(my_list[idx - 1], element)
+        #del my_list[idx]
+        #new_list = my_list.insert(my_list[idx - 1], element)
+        my_list[idx] = element
         return my_list
