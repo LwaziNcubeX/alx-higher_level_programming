8bf2594c6641b058075bae8100ac1894d1774891
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-13 17:47:48 +0200

    testing

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx >= 0 and idx <= (len(my_list)):
-        del my_list[idx]
-        new_list = my_list.insert(my_list[idx - 1], element)
-#        my_list[idx] = element
+#        del my_list[idx]
+#        new_list = my_list.insert(my_list[idx - 1], element)
+        my_list[idx] = element
         return my_list
