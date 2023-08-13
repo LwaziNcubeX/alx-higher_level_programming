cc79c9830c9bdc65416623e2b7f17c794f510272
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-13 14:41:24 +0200

    fixed if idx if out of range

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    print(len(my_list))
+
     if idx < 0:
         return None
-    elif idx > len(my_list):
+    elif idx > (len(my_list) - 1):
         return None
     else:
         return idx + 1
