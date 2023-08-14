67b8ee03c288b38ff2c5b5456b5923081757898f
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-14 17:08:55 +0200

    change 0 to my_list[0]

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        max_value = 0
+        max_value = my_list[0]
         for x in my_list:
             if x > max_value:
                 max_value = x
