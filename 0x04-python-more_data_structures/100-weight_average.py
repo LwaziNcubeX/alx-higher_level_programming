9c20ae88d4e9c68a9a5c8c92ee70ebaa4aae97fb
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-17 04:19:23 +0200

    A function to calculate weighted average

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    num = sum(map(lambda x: x[0] * x[1], my_list))
+    denom = sum(map(lambda x: x[1], my_list))
+    return num / denom
