312d483f4a103ae9d00d137acce8530fb2c954d9
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-17 02:41:12 +0200

    "Refactor `update_dictionary` function for efficiency and clarity"

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    update_dict = dict(map(lambda x: (key, value) if x[0] == key else x, a_dictionary.items()))
-    a_dictionary.update(update_dict)
+    a_dictionary[key] = value
     return a_dictionary
