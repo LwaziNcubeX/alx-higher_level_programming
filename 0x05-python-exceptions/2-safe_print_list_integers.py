83db1f79ce152c43d98417f79f952826fb0484df
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-29 00:54:26 +0200

    removed unecessary code

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    count_elements = lambda i: sum(1 for _ in i)
-    total = count_elements(my_list) - 1
     for i in range(x):
         try:
-            if isinstance(my_list[i], int) and my_list[i] <= total:
-                print("{:d}".format(my_list[i]), end="")
-                count += 1
-        except Exception:
-            raise
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (TypeError, ValueError):
+            pass
     print()
     return count
