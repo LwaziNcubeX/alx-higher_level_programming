c8783b7e2f517a7d7cc40c878e320bf0e0aa2e3c
LwaziNcubeX <lwazincubex@gmail.com>
2023-08-29 23:49:39 +0200

    Added a detailed docString

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 class Square:
-    """
-        This is an empty class that defines a square
-        without importing any modules
+    """This is an empty class that defines a square.
+
+    A square is a closed shape with four equal sides and four right angles.
+    This class represents a square shape as an object that can be used in
+    a larger program.
+
+    Attributes:
+        There are no attributes for this class.
+
+    Methods:
+        There are no methods for this class yet, but it can be expanded to
+        include functions such as
+        calculating area, perimeter, or drawing the square.
     """
     pass
