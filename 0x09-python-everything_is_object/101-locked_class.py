5b32e06d060e5b7f2f48e49c59ed76db1b62e0ff
LwaziNcubeX <lwazincubex@gmail.com>
2023-09-06 00:34:15 +0200

    added docs

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,13 +1,25 @@
 #!/usr/bin/python3
+""" LOCKED CLASS """
+
+
 class LockedClass:
-    """
-    Define the __slots__ attribute to only 
-    allow a single instance attribute
-    """
+    """A class that only allows a single, pre-defined attribute."""
+
     __slots__ = ['first_name']
+    """The __slots__ attribute is used to optimize memory usage.
+    This class has only one allowed attribute, 'first_name'."""
 
     def __setattr__(self, name, value):
+        """Override the __setattr__ method to prevent dynamic creation
+        of attributes except for the allowed 'first_name' attribute."""
+
         if not hasattr(self, name) and name != 'first_name':
+            """Check if the attribute being set is not already defined
+            and is not the allowed 'first_name' attribute."""
+
             raise AttributeError(
                     "'LockedClass' object has no attribute '" + name + "'")
+
         super().__setattr__(name, value)
+        """Set the attribute and its value using the parent class's
+        implementation of __setattr__."""
