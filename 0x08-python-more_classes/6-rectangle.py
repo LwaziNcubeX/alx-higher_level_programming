f17c94fedde4813319387ad306703d7973346f32
LwaziNcubeX <lwazincubex@gmail.com>
2023-09-05 02:05:46 +0200

    added instances count (based on 5-rectangle.py)

---
+++ 0x08-python-more_classes/6-rectangle.py
@@ -0,0 +1,142 @@
+#!/usr/bin/python3
+"""Detect instance deletion"""
+
+
+class Rectangle:
+    """
+    An empty class that defines a Rectangle
+
+    Args:
+        0
+
+    Modules:
+        None
+    """
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """
+        Instantiates an object of this class
+
+        Args:
+            width (int, optional): Defaults to 0 if not provided.
+            height (int, optional): Defaults to 0 if not provided.
+        """
+        self.height = height
+        self.width = width
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """
+        Private instance attribute: width
+        #READ ONLY
+
+        Returns:
+            self._width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Assigns a new variable for width
+        that can be changed
+        Args:
+            value:
+
+        Returns:
+            Nothing
+        """
+
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif isinstance(value, int):
+            if value < 0:
+                raise ValueError("width must be >= 0")
+            else:
+                self.__width = value
+
+    @property
+    def height(self):
+        """
+        Private instance attribute: width
+        #READ ONLY
+
+        Returns:
+            height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Assigns a new variable for height
+        that can be changed
+
+        Args:
+            value:
+
+        Returns:
+            Nothing
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif isinstance(value, int):
+            if value < 0:
+                raise ValueError("height must be >= 0")
+            else:
+                self.__height = value
+
+    def area(self):
+        """
+        calculates area of a rectangle
+
+        Returns:
+            area
+        """
+        return self.height * self.width
+
+    def perimeter(self):
+        """
+        calculates perimeter of a rec
+        Returns:
+            0 if width or height = 0
+            else:
+                total perimeter
+        """
+        if self.width == 0 or self.height == 0:
+            return 0
+        else:
+            return (self.height + self.width) * 2
+
+    def __str__(self):
+        """
+        prints the rectangle with the char #
+
+        Returns:
+            empty string if width or height = 0
+            else:
+                prints str #
+        """
+        if self.width == 0 or self.height == 0:
+            return ""
+        else:
+            output = []
+            for i in range(self.height):
+                output.append("#" * self.width)
+            return "\n".join(output)
+
+    def __repr__(self):
+        """
+        string representation of the object
+
+        Returns:
+            a string
+
+        """
+        return f"Rectangle({self.width}, {self.height})"
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
