58b921a4060a8aff5156d607dfdb3ffd3f163ade
LwaziNcubeX <lwazincubex@gmail.com>
2023-09-12 00:18:52 +0200

    A class Rectangle that inherits from BaseGeometry (7-base_geometry.py).

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+""" Improve Geometry """
+
+
+class BaseGeometry:
+    """
+    A class BaseGeometry (based on 6-base_geometry.py).
+
+    :raise Exception, TypeError, ValueError
+    :return Nothing
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    @staticmethod
+    def integer_validator(name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class Rectangle that inherits from BaseGeometry (7-base_geometry.py).
+
+    width: The width of the rectangle (must be a positive integer)
+    height: The height of the rectangle (must be a positive integer)
+
+    return: An instance of Rectangle
+    """
+
+    def __init__(self, width, height):
+        """
+        Initializes a Rectangle instance with the given width and height.
+
+        width: The width of the rectangle (must be a positive integer)
+        height: The height of the rectangle (must be a positive integer)
+        raises ValueError: If either width or
+            height is not a positive integer
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
