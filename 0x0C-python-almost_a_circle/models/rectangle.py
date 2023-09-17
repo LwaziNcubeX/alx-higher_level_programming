253bcb141c55e8fe1a52549083e1ae48a48b1f15
LwaziNcubeX <lwazincubex@gmail.com>
2023-09-17 02:11:50 +0200

    added a public method that assigns args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -159,6 +159,20 @@ class Rectangle(Base):
             for j in range(self.width):
                 print("#", end="")
             print("")
+
     def __str__(self):
         return "[Rectangle] ({}) {}/{} - {}/{}"\
             .format(self.id, self.__x, self.__y, self.__width, self.__height)
+
+    def update(self, *args):
+        """assign each argument to attribute"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
