8831da2e16cf9ba3e1fa3c7d703daba2e943e710
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-14 01:31:44 +0200

    removed const line and black lines

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
-
 const process = require('node:process');
-const arg = process.argv[2];
 
 if (process.argv[2] === undefined) {
   console.log('No argument');
-} else if (arg > process.argv[1]) {
-  console.log(arg);
+} else if (process.argv[2] > process.argv[1]) {
+  console.log(process.argv[2]);
 }
