b7ae013f75b0bb52f21e3be74907545b59fbed62
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-14 01:23:12 +0200

    restore old commit

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-console.log(process.argv.length === 2 ? 'Argument found' : process.argv.length > 2 ? 'Arguments found' : 'No argument');
+if (process.argv.length === 2) {
+    console.log('No argument');
+} else if (process.argv.length === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
