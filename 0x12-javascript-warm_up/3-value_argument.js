817f557083df1a2aab2dfe50c5f013be6081eaf3
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-14 01:39:00 +0200

    updated if statement to use typeof

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-const process = require('node:process');
 
-if (process.argv[2] === undefined) {
+if (typeof process.argv[2] === 'undefined') {
   console.log('No argument');
-} else if (process.argv[2] > process.argv[1]) {
+} else {
   console.log(process.argv[2]);
 }
