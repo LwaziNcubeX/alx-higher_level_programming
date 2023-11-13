f7229305be2b77c65dd4456b8a5bfc4cfe2c94a9
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-14 00:47:18 +0200

    A script that prints the first argument passed to it.

---
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const process = require('node:process');
+const arg = process.argv[2];
+
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else if (arg > process.argv[1]) {
+  console.log(arg);
+}
