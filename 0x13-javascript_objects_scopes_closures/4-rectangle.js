75ccc13b43e96e1d3c5e46c0f7f0af5ef4813b67
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-14 23:52:29 +0200

    Create an instance method (double & rotate)

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      let j = 0;
+      let row = '';
+
+      while (j < this.width) {
+        row += 'X';
+        j++;
+      }
+
+      console.log(row);
+      i++;
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+module.exports = Rectangle;
