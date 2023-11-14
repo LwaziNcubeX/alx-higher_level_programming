9b6d81aabbafed4fc26441ee0f0f73d583fcbab6
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-14 02:10:26 +0200

    removed redundant code

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-if (typeof process.argv[2] === 'undefined' || isNaN(parseInt(process.argv[2]))) {
+if (!parseInt(process.argv[2])) {
   console.log('Not a number');
 } else {
   console.log('My number: ', parseInt(process.argv[2]));
