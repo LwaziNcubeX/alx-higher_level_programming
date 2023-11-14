fc7789feceec95d5b3ed60999a4ad0df24da0ca8
LwaziNcubeX <lwazincubex@gmail.com>
2023-11-14 02:13:48 +0200

    fix the else statement to display num as a str

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 
-if (!parseInt(process.argv[2])) {
+const arg = process.argv[2];
+
+if (!parseInt(arg)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', parseInt(process.argv[2]));
+  console.log(`My number: ${parseInt(arg)}`);
 }
