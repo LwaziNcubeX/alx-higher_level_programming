35de14aa84bc5a45d6cb2ccaeb89029816cf3cb7
LwaziNcubeX <lwazincubex@gmail.com>
2023-12-02 01:18:59 +0200

    A Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A Python script that fetches https://alx-intranet.hbtn.io/status"""
+from urllib import request
+
+
+def fetch_me_again():
+    """
+    A Function that fetches https://alx-intranet.hbtn.io/status
+    """
+    url = "https://alx-intranet.hbtn.io/status"
+    with request.urlopen(url) as response:
+        data = response.read().decode('utf8')
+        print('Body response:')
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+
+
+if __name__ == "__main__":
+    fetch_me_again()
