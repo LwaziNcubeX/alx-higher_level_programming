d2c516034be97f450c13381689a1bd84e76d0e38
Lwazi Ncube <lwazincubex@gmail.com>
2024-01-04 02:07:42 +0200

    remove custom error

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,6 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const string = process.argv[3];
 
-if (!filePath || !string) {
-  console.error('Usage: ./1-writeme.js <file-path> <string>');
-  process.exit(1);
-}
-
 fs.writeFile(filePath, string, 'utf-8', (err) => {
   if (err) {
     console.error(err);
